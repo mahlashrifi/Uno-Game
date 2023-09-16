4509a0f084fdbc21364782647d085db598d19a74
mahlashrifi <mahla7997@gmail.com>
2023-09-16 23:39:00 +0330

    Add Main class

---
+++ Main.java
@@ -0,0 +1,14 @@
+package uno;
+
+import uno.conroller.Manager;
+import uno.conroller.Run;
+import uno.model.Cards.*;
+import uno.model.Repository;
+import uno.view.Show;
+
+public class Main {
+    public static void main(String[] args) {
+        Run run = new Run();
+        run.runGame();
+     }
+}
